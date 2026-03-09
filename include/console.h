683c68f400416af493e1e40c160f81724a2fcd6b
shockpast <shockpast@gmail.com>
2026-03-09 12:12:25 +0300

    feat: initial commit

---
+++ include/console.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <string_view>
+#include <format>
+#include <print>
+#include <source_location>
+#include <utility>
+
+namespace console {
+    namespace detail {
+        namespace color {
+            inline constexpr std::string_view reset = "\033[0m";
+            inline constexpr std::string_view bold = "\033[1m";
+            inline constexpr std::string_view dim = "\033[2m";
+
+            inline constexpr std::string_view black = "\033[30m";
+            inline constexpr std::string_view red = "\033[31m";
+            inline constexpr std::string_view green = "\033[32m";
+            inline constexpr std::string_view yellow = "\033[33m";
+            inline constexpr std::string_view blue = "\033[34m";
+            inline constexpr std::string_view magenta = "\033[35m";
+            inline constexpr std::string_view cyan = "\033[36m";
+            inline constexpr std::string_view white = "\033[37m";
+        } // namespace color
+
+        enum class level { trace, debug, info, warn, error, fatal };
+
+        inline level g_min_level = level::trace;
+
+        [[nodiscard]]
+        constexpr std::string_view level_label(level lvl) noexcept {
+            switch (lvl) {
+            case level::trace: return "TRACE";
+            case level::debug: return "DEBUG";
+            case level::info:  return "INFO";
+            case level::warn:  return "WARN";
+            case level::error: return "ERROR";
+            case level::fatal: return "FATAL";
+            }
+            std::unreachable();
+        }
+
+        [[nodiscard]]
+        constexpr std::string_view level_color(level lvl) noexcept {
+            switch (lvl) {
+            case level::trace: return color::dim;
+            case level::debug: return color::cyan;
+            case level::info:  return color::green;
+            case level::warn:  return color::yellow;
+            case level::error: return color::red;
+            case level::fatal: return color::red;
+            }
+            std::unreachable();
+        }
+
+        inline void emit(level lvl, std::string_view msg, const std::source_location loc = std::source_location::current()) {
+            if (lvl < g_min_level) return;
+
+            const auto color = level_color(lvl);
+            const auto label = level_label(lvl);
+
+            std::println("{}[{}]{} {} {}({}:{}){} ", color, label, color::reset, msg, color::dim, loc.file_name(), loc.line(), color::reset);
+        }
+    } // namespace detail
+
+    inline void set_level(detail::level lvl) noexcept {
+        detail::g_min_level = lvl;
+    }
+
+    template<typename... Args>
+    inline void trace(std::format_string<Args...> fmt, Args&&... args, const std::source_location loc = std::source_location::current()) {
+        detail::emit(detail::level::trace, std::format(fmt, std::forward<Args>(args)...), loc);
+    }
+
+    template<typename... Args>
+    inline void debug(std::format_string<Args...> fmt, Args&&... args, const std::source_location loc = std::source_location::current()) {
+        detail::emit(detail::level::debug, std::format(fmt, std::forward<Args>(args)...), loc);
+    }
+
+    template<typename... Args>
+    inline void info(std::format_string<Args...> fmt, Args&&... args, const std::source_location loc = std::source_location::current()) {
+        detail::emit(detail::level::info, std::format(fmt, std::forward<Args>(args)...), loc);
+    }
+
+    template<typename... Args>
+    inline void warn(std::format_string<Args...> fmt, Args&&... args, const std::source_location loc = std::source_location::current()) {
+        detail::emit(detail::level::warn, std::format(fmt, std::forward<Args>(args)...), loc);
+    }
+
+    template<typename... Args>
+    inline void error(std::format_string<Args...> fmt, Args&&... args, const std::source_location loc = std::source_location::current()) {
+        detail::emit(detail::level::error, std::format(fmt, std::forward<Args>(args)...), loc);
+    }
+
+    template<typename... Args>
+    inline void fatal(std::format_string<Args...> fmt, Args&&... args, const std::source_location loc = std::source_location::current()) {
+        detail::emit(detail::level::fatal, std::format(fmt, std::forward<Args>(args)...), loc);
+    }
+} // namespace console
---
+++ include/math.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <cmath>
+
+namespace math
+{
+    class vector2
+    {
+    public:
+        float x, y;
+
+        constexpr vector2() noexcept : x(0), y(0) {}
+        constexpr vector2(float x, float y) noexcept : x(x), y(y) {}
+
+        [[nodiscard]] constexpr vector2 operator+(const vector2& v) const noexcept { return { x + v.x, y + v.y }; }
+        [[nodiscard]] constexpr vector2 operator-(const vector2& v) const noexcept { return { x - v.x, y - v.y }; }
+        [[nodiscard]] constexpr vector2 operator*(const vector2& v) const noexcept { return { x * v.x, y * v.y }; }
+        [[nodiscard]] constexpr vector2 operator/(const vector2& v) const noexcept { return { x / v.x, y / v.y }; }
+
+        [[nodiscard]] constexpr vector2 operator*(float s) const noexcept { return { x * s, y * s }; }
+        [[nodiscard]] constexpr vector2 operator/(float s) const noexcept { return { x / s, y / s }; }
+
+        constexpr vector2& operator+=(const vector2& v) noexcept { x += v.x; y += v.y; return *this; }
+        constexpr vector2& operator-=(const vector2& v) noexcept { x -= v.x; y -= v.y; return *this; }
+        constexpr vector2& operator*=(float s)          noexcept { x *= s;   y *= s;   return *this; }
+        constexpr vector2& operator/=(float s)          noexcept { x /= s;   y /= s;   return *this; }
+
+        [[nodiscard]] constexpr vector2 operator-() const noexcept { return { -x, -y }; }
+
+        [[nodiscard]] constexpr float dot(const vector2& v) const noexcept { return x * v.x + y * v.y; }
+        [[nodiscard]] constexpr float cross(const vector2& v) const noexcept { return x * v.y - y * v.x; }
+        [[nodiscard]] constexpr vector2 perp() const noexcept { return { -y, x }; }
+
+        [[nodiscard]] constexpr float length_squared() const noexcept { return dot(*this); }
+        [[nodiscard]] float length() const noexcept { return std::sqrt(length_squared()); }
+        [[nodiscard]] vector2 normalized() const noexcept { return *this / length(); }
+        [[nodiscard]] float angle() const noexcept { return std::atan2(y, x); }
+
+        [[nodiscard]] constexpr bool operator==(const vector2&) const noexcept = default;
+    };
+
+	class vector3
+	{
+	public:
+		float x, y, z;
+
+		constexpr vector3() noexcept : x(0.0), y(0.0), z(0.0) {};
+		constexpr vector3(float x, float y, float z) noexcept : x(x), y(y), z(z) {};
+
+		[[nodiscard]] constexpr vector3 operator+(const vector3& v) const noexcept { return { x + v.x, y + v.y, z + v.z }; }
+		[[nodiscard]] constexpr vector3 operator-(const vector3& v) const noexcept { return { x - v.x, y - v.y, z - v.z }; }
+		[[nodiscard]] constexpr vector3 operator*(const vector3& v) const noexcept { return { x * v.x, y * v.y, z * v.z }; }
+		[[nodiscard]] constexpr vector3 operator/(const vector3& v) const noexcept { return { x / v.x, y / v.y, z / v.z }; }
+		
+		[[nodiscard]] constexpr vector3 operator*(float s) const noexcept { return { x * s, y * s, z * s }; }
+		[[nodiscard]] constexpr vector3 operator/(float s) const noexcept { return { x / s, y / s, z / s }; }
+
+		constexpr vector3& operator+=(const vector3& v) noexcept { x += v.x; y += v.y; z += v.z; return *this; }
+		constexpr vector3& operator-=(const vector3& v) noexcept { x -= v.x; y -= v.y; z -= v.z; return *this; }
+		constexpr vector3& operator*=(float s)          noexcept { x *= s;   y *= s;   z *= s;   return *this; }
+
+		[[nodiscard]] constexpr vector3 operator-() const noexcept { return { -x, -y, -z }; }
+
+		[[nodiscard]] constexpr float dot(const vector3& v) const noexcept { return x * v.x + y * v.y + z * v.z; }
+		[[nodiscard]] constexpr vector3 cross(const vector3& v) const noexcept { return { y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x }; }
+		
+        [[nodiscard]] float length() const noexcept { return std::sqrt(dot(*this)); }
+		[[nodiscard]] vector3 normalized() const noexcept { return *this / length(); }
+
+		[[nodiscard]] constexpr bool operator==(const vector3&) const noexcept = default;
+	};
+}
